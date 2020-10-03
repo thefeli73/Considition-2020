16988ababbe87b31ae76f486354a9fdbfcd1f66a
Thefeli73 <thefeli73@hotmail.com>
2020-10-03 19:30:14 +0200

    flutta game state var

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ api_key = "74e3998d-ed3d-4d46-9ea8-6aab2efd8ae3"
 map_name = "training1"  # TODO: You map choice here. If left empty, the map "training1" will be selected.
 
 game_layer = GameLayer(api_key)
+state = game_layer.game_state
 usePrebuiltStrategy = False
 
 
