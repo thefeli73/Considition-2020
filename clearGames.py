b9ea513f50562a5a566b12ee32a52914d47e0903
Thefeli73 <thefeli73@hotmail.com>
2020-10-04 04:58:59 +0200

    clear all active games

---
+++ clearGames.py
@@ -0,0 +1,8 @@
+from game_layer import GameLayer
+api_key = "74e3998d-ed3d-4d46-9ea8-6aab2efd8ae3"
+game_layer = GameLayer(api_key)
+state = game_layer.game_state
+game_layer.force_end_game()
+game_layer.force_end_game()
+game_layer.force_end_game()
+game_layer.force_end_game()
