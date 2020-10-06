d3a4849fd16690dc14b519ce2bba1c4a097de91c
Thefeli73 <thefeli73@hotmail.com>
2020-10-07 00:43:39 +0200

    patch launcher

--- launcher.py
+++ launcher.py
@@ -1,8 +1,8 @@
 import main
-number_of_launches = 6
+number_of_launches = 4
 result_list = []
 for i in range(number_of_launches):
     result_list.append(main.main())
 for result in result_list:
     print("Game " + result[0] + " had a score of: " + str(result[1]))
-    input("Press Enter to exit")
+input("Press Enter to exit")
