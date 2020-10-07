cf977331e36c42b892c77ee4a4ec57087e45ebdb
Thefeli73 <thefeli73@hotmail.com>
2020-10-07 03:34:42 +0200

    PARALLELISERAD LAUNCHER!!!

--- launcher.py
+++ launcher.py
@@ -1,8 +1,23 @@
 import main
-number_of_launches = 4
-result_list = []
-for i in range(number_of_launches):
-    result_list.append(main.main())
-for result in result_list:
-    print("Game " + result[0] + " had a score of: " + str(result[1]))
-input("Press Enter to exit")
+import time
+from multiprocessing import Pool
+import multiprocessing
+
+proc_running = 4  # MAX 4!!!
+
+
+def run_main(n):
+    result = main.main()
+    return result
+
+
+def launch(list):
+    for result in list:
+        print("Game " + result[0] + " had a score of: " + str(result[1]))
+    input("Press Enter to exit")
+
+
+if __name__ == '__main__':
+    with Pool(proc_running) as p:
+        results = p.map(run_main, range(proc_running))
+    launch(results)
